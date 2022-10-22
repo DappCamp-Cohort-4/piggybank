a8baa0ad3db87339fadf3bd77a14721fff4d0b18
Rajesh Muppalla <1348422+codingnirvana@users.noreply.github.com>
2022-10-22 14:11:44 +0530

    Adding a test to check owner assignment

--- src/PiggyBank.sol
+++ src/PiggyBank.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.13;
 
 contract PiggyBank {
     address child;
-    address owner;
-    uint maxWithdrawAmount;
+    address public owner;
+    uint256 maxWithdrawAmount;
 
-    constructor(address _child, uint _maxWithdrawAmount) {
+    constructor(address _child, uint256 _maxWithdrawAmount) {
         owner = msg.sender;
         child = _child;
         maxWithdrawAmount = _maxWithdrawAmount;
@@ -17,13 +17,12 @@ contract PiggyBank {
         _;
     }
 
-    function deposit() payable public {
-    }
+    function deposit() public payable {}
 
-    function withdraw(uint amount) public isChild() {
-        require(amount <= maxWithdrawAmount,"Withdraw amount greater than balance");
+    function withdraw(uint256 amount) public isChild {
+        require(amount <= maxWithdrawAmount, "Withdraw amount greater than balance");
         require(amount <= address(this).balance, "Amount greater than balance");
-        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        (bool success,) = payable(msg.sender).call{value: amount}("");
         require(success, "Failed to send Ether");
     }
 }
--- test/PiggBank.t.sol
+++ test/PiggBank.t.sol
@@ -6,41 +6,45 @@ import "../src/PiggyBank.sol";
 
 contract PiggyBankTest is Test {
     PiggyBank public piggyBank;
-    address childAddress = address(0xDEAD);
-    address unauthorizedAddress = address(0xBEEF);
+    address andrew = address(0xABCD);
+    address betty = address(0xDEAD);
+    address unauthorized = address(0xBEEF);
     uint256 maxWithDrawlAmount = 2 ether;
 
     function setUp() public {
-       piggyBank = new PiggyBank(childAddress, maxWithDrawlAmount);     
-       piggyBank.deposit {value: 10 ether}();
-       assertEq(address(piggyBank).balance, 10 ether);  
+        vm.prank(andrew);
+        piggyBank = new PiggyBank(betty, maxWithDrawlAmount);
+        piggyBank.deposit{value: 10 ether}();
+        assertEq(address(piggyBank).balance, 10 ether);
+    }
+
+    function testDeployerIsSetToOwner() public {
+        assertEq(piggyBank.owner(), address(andrew));
     }
 
     function testSuccessfulWithdraw() public {
-      vm.prank(childAddress);
-      piggyBank.withdraw(1 ether);
-      assertEq(address(piggyBank).balance, 9 ether);
-      assertEq(address(childAddress).balance, 1 ether);   
+        vm.prank(betty);
+        piggyBank.withdraw(1 ether);
+        assertEq(address(piggyBank).balance, 9 ether);
+        assertEq(address(betty).balance, 1 ether);
     }
 
-    function testCannotWithdrawIfNotChild() public {
-        vm.prank(unauthorizedAddress);
+    function testCannotWithdrawIfNotBetty() public {
+        vm.prank(unauthorized);
         vm.expectRevert("Only the child can withdraw");
         piggyBank.withdraw(1 ether);
     }
 
     function testCannotWithdrawOverLimit() public {
-        vm.prank(childAddress);
+        vm.prank(betty);
         vm.expectRevert("Withdraw amount greater than balance");
-        piggyBank.withdraw(3 ether);        
+        piggyBank.withdraw(3 ether);
     }
 
     function testCannotWithdrawMoreThanAvailableBalance() public {
-        vm.prank(childAddress);
+        vm.prank(betty);
         vm.deal(address(piggyBank), 1 ether);
         vm.expectRevert("Amount greater than balance");
         piggyBank.withdraw(1.5 ether);
     }
-
-    
 }
